e62060716b5bcee3cf6c5d8bcbc954952fc65785
RubenTmns <terminasyan.ruben@gmail.com>
2021-03-11 12:06:11 +0100

    fix sparta

--- 02_programming/01_algorithms_day_1/ac70f6144dcf84755277ce62ada7a2ee9726bf17/01_play_with_variables/src/01_play_with_variables.js
+++ 02_programming/01_algorithms_day_1/ac70f6144dcf84755277ce62ada7a2ee9726bf17/01_play_with_variables/src/01_play_with_variables.js
@@ -1,12 +1,17 @@
 // a constant called `question`, bound to a string: "Sparta, what is your profession?!";
+const question = "Spartan, what is your profession?!";
 
 // a constant called `answer`, bound to a string: "Dev dev dev!";
+const answer = "Dev dev dev!";
 
 // a constant called `foo`, bound to an integer: 12;
+const foo = 12;
 
 // a constant called `bar`, bound to an integer: 28;
+const bar = 28;
 
 // a constant called `sumResult`, bound to the sum of `foo` and `bar`;
+const sumResult = foo + bar;
 
 // a constant called `prodResult`, bound to the product of `bar` and `foo`.
 
